1925ac3cc5483a54ad42cca7c9030f255a9f1070
Erdem <erdemsincer123@icloud.com>
2025-05-22 01:22:38 +0300

    contex sınıfı

---
+++ services/DreamService/DreamService.Infrastructure/Data/DreamDbContext.cs
@@ -0,0 +1,25 @@
+﻿using DreamService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace DreamService.Infrastructure.Data;
+
+public class DreamDbContext : DbContext
+{
+    public DreamDbContext(DbContextOptions<DreamDbContext> options)
+        : base(options) { }
+    public DbSet<Dream> Dreams => Set<Dream>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Dream>(entity =>
+        {
+            entity.HasKey(d => d.Id);
+            entity.Property(d => d.Text).IsRequired();
+            
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
