475872e8cc1dcc042663e33ad5a8974f24d46136
Erdem <erdemsincer123@icloud.com>
2025-05-21 21:37:29 +0300

    contex sınıfı

---
+++ services/AuthService/AuthService.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AuthService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(u => u.Id);
+            entity.Property(u => u.Username).IsRequired();
+            entity.Property(u => u.Email).IsRequired();
+            entity.Property(u => u.PasswordHash).IsRequired();
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
