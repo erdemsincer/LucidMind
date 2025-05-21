35a003f79282b92120dfe3a56939fb74d21e4d12
Erdem <erdemsincer123@icloud.com>
2025-05-21 21:37:07 +0300

    user entity

---
+++ services/AuthService/AuthService.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthService.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    public string Role { get; set; } = "User";  // User | Admin
+}
