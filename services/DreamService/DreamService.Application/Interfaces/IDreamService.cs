55e31f72cc172209b74b0c53f9a7d88c06305005
Erdem <erdemsincer123@icloud.com>
2025-05-22 01:22:30 +0300

    dream service

---
+++ services/DreamService/DreamService.Application/Interfaces/IDreamService.cs
@@ -0,0 +1,11 @@
+﻿using DreamService.Application.Dtos;
+
+namespace DreamService.Application.Interfaces;
+
+public interface IDreamService
+{
+    Task<string> CreateAsync(CreateDreamDto dto, int userId);
+
+    Task<List<ResultDreamDto>> GetAllAsync(int userId);
+    Task<ResultDreamDto?> GetByIdAsync(int id);
+}
