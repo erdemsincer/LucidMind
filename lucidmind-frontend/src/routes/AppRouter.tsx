e7f9bb2a0d0abd94326d3b2156c7b23085d186b5
Erdem <erdemsincer123@icloud.com>
2025-05-22 02:34:51 +0300

    rputes ayarları güncelelme

--- lucidmind-frontend/src/routes/AppRouter.tsx
+++ lucidmind-frontend/src/routes/AppRouter.tsx
@@ -1,14 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
+import HomePage from "../pages/HomePage";
+import MyDreamsPage from "../pages/MyDreamsPage";
 import Layout from "../components/Layout";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<div className="text-center p-4">Hoş Geldin!</div>} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dreams"
+            element={
+              <ProtectedRoute>
+                <MyDreamsPage />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Route>
