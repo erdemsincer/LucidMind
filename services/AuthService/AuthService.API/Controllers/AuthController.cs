fa0cf27ce05209673269a9c7e4e031efa5e7d0f7
Erdem <erdemsincer123@icloud.com>
2025-05-21 21:38:29 +0300

    auth controller

---
+++ services/AuthService/AuthService.API/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using AuthService.Application.Dtos.Auth;
+using AuthService.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterDto dto)
+    {
+        var result = await _authService.RegisterAsync(dto);
+        return Ok(result);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto dto)
+    {
+        var result = await _authService.LoginAsync(dto);
+        return Ok(result);
+    }
+}
